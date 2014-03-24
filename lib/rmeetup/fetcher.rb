3a1e68ab7c09b66f630380815ba5fee8b3d4a491
Nikica Jokic <neektza@gmail.com>
2014-03-24 15:51:41 +0100

    Dispatch to fetcher in Fetcher.for

--- lib/rmeetup/fetcher.rb
+++ lib/rmeetup/fetcher.rb
@@ -4,6 +4,7 @@ require "rmeetup/fetcher/cities"
 require "rmeetup/fetcher/members"
 require "rmeetup/fetcher/rsvps"
 require "rmeetup/fetcher/events"
+require "rmeetup/fetcher/open_events"
 require "rmeetup/fetcher/groups"
 require "rmeetup/fetcher/comments"
 require "rmeetup/fetcher/photos"
@@ -11,30 +12,32 @@ require "rmeetup/fetcher/venues"
 
 module RMeetup
   module Fetcher
-    
+
     class << self
       # Return a fetcher for given type
       def for(type)
-        return  case type.to_sym
-                when :topics
-                  Topics.new
-                when :cities      
-                  Cities.new
-                when :members     
-                  Members.new
-                when :rsvps       
-                  Rsvps.new
-                when :events      
-                  Events.new
-                when :groups      
-                  Groups.new
-                when :comments    
-                  Comments.new
-                when :photos
-                  Photos.new
-					 when :venues
-						Venues.new
-                end
+        case type.to_sym
+        when :topics
+          Topics.new
+        when :cities      
+          Cities.new
+        when :members     
+          Members.new
+        when :rsvps       
+          Rsvps.new
+        when :events      
+          Events.new
+        when :groups      
+          Groups.new
+        when :comments    
+          Comments.new
+        when :photos
+          Photos.new
+        when :open_events
+          OpenEvents.new
+        when :venues
+          Venues.new
+        end
       end 
     end
   end
