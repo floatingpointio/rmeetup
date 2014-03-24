e56d895e90df6302ac8988a07f0a747c3a98a381
Nikica Jokic <neektza@gmail.com>
2014-03-24 17:04:34 +0100

    String#camel_case in one line

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,11 +1,6 @@
 class String
   def snake_case
-    self.gsub(/::/, '/')
-      .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-      .gsub(/([a-z\d])([A-Z])/,'\1_\2')
-      .tr("-", "_")
-      .tr(".", "_")
-      .downcase
+    self.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').gsub(/([a-z\d])([A-Z])/,'\1_\2').tr("-", "_").tr(".", "_").downcase
   end
 
   def camel_case
