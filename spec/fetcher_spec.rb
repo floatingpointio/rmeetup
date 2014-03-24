b9fe23aafedcd3f1a9d8d6f02e7d2ee4350767f8
Nikica Jokic <neektza@gmail.com>
2014-03-24 16:16:22 +0100

    Updated specs.

--- spec/fetcher_spec.rb
+++ spec/fetcher_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe RMeetup::Fetcher, 'being told to get a certain type of fetcher' do
   before do
-    @fetcher_types = %w(topics cities members rsvps events groups comments photos)
+    @fetcher_types = %w(topics cities members rsvps events groups comments photos open_events)
   end
   
   it 'should return the correct fetcher' do
     @fetcher_types.each do |type|
       fetcher = RMeetup::Fetcher.for(type)
-      fetcher.class.name.should eql("RMeetup::Fetcher::#{type.capitalize}")
+      expect(fetcher.class.name).to eq("RMeetup::Fetcher::#{type.camel_case}")
     end
   end
   
   it 'should return nil if asked for an invalid fetcher' do
-    RMeetup::Fetcher.for(:clowns).should be_nil
+    expect { RMeetup::Fetcher.for(:clowns) }.to raise_error
   end
 end
