db7078ccfffdae9b9a212bbfc25e1e01ef489427
Nikica Jokic <neektza@gmail.com>
2014-03-24 16:09:37 +0100

    Dynamically get appropriate fetcher type.

--- lib/rmeetup/fetcher.rb
+++ lib/rmeetup/fetcher.rb
@@ -1,44 +1,24 @@
-require "rmeetup/fetcher/base"
-require "rmeetup/fetcher/topics"
-require "rmeetup/fetcher/cities"
-require "rmeetup/fetcher/members"
-require "rmeetup/fetcher/rsvps"
-require "rmeetup/fetcher/events"
-require "rmeetup/fetcher/open_events"
-require "rmeetup/fetcher/groups"
-require "rmeetup/fetcher/comments"
-require "rmeetup/fetcher/photos"
-require "rmeetup/fetcher/venues"
+require 'core_ext/string'
+require 'rmeetup/fetcher/base'
+require 'rmeetup/fetcher/topics'
+require 'rmeetup/fetcher/cities'
+require 'rmeetup/fetcher/members'
+require 'rmeetup/fetcher/rsvps'
+require 'rmeetup/fetcher/events'
+require 'rmeetup/fetcher/open_events'
+require 'rmeetup/fetcher/groups'
+require 'rmeetup/fetcher/comments'
+require 'rmeetup/fetcher/photos'
+require 'rmeetup/fetcher/venues'
 
 module RMeetup
   module Fetcher
 
-    class << self
-      # Return a fetcher for given type
-      def for(type)
-        case type.to_sym
-        when :topics
-          Topics.new
-        when :cities      
-          Cities.new
-        when :members     
-          Members.new
-        when :rsvps       
-          Rsvps.new
-        when :events      
-          Events.new
-        when :groups      
-          Groups.new
-        when :comments    
-          Comments.new
-        when :photos
-          Photos.new
-        when :open_events
-          OpenEvents.new
-        when :venues
-          Venues.new
-        end
+    def self.for(type)
+      if (type && fetcher = const_get(type.to_s.camel_case.to_sym))
+        fetcher
       end 
     end
+
   end
 end
