8e64e0d9600cc40ea885905c627b7a71312bec00
Nikica Jokic <neektza@gmail.com>
2014-04-22 16:01:28 +0200

    :access_token authorization supported.

--- lib/rmeetup/client.rb
+++ lib/rmeetup/client.rb
@@ -13,7 +13,7 @@ module RMeetup
   # Needs to be the group organizers API Key
   # to be able to RSVP for other people
   class Configuration
-    attr_accessor :api_key
+    attr_accessor :api_key, :access_token
   end
 
   # == RMeetup::Client
@@ -24,10 +24,16 @@ module RMeetup
   class Client
     # Initializes a new Client object
     #
-    # @param type [Symbol] A type of fetcher we're going to use. You use it to define what data do you want from meetup.com
+    # @param type [Hash] Options, currenlty only for providing clinet with the auth data
+    # @yield type [Configuration], object with accessors :api_key and :access_token, used for providing auth data in a backward compatible way
     # @return [RMeetup::Client]
-    def initialize
-      yield(configuration) if block_given?
+    def initialize(opts = {})
+      if not(opts.empty?)
+        configuration.api_key = opts[:api_key] if opts.has_key?(:api_key)
+        configuration.access_token = opts[:access_token] if opts.has_key?(:access_token)
+      else
+        yield(configuration) if block_given?
+      end
       check_configuration!
     end
 
@@ -50,14 +56,18 @@ module RMeetup
     end
 
     def auth
-      { :key => configuration.api_key }
+      if configuration.api_key
+        { :key => configuration.api_key, :sign => 'true' }
+      elsif configuration.access_token
+        { :access_token => configuration.access_token }
+      end
     end
 
     # Ensures that API key is set during configuration.
     #
     # @raise [RMeetup::Error::ConfigurationError] Error is raised when API key isn't provided
     def check_configuration!
-      fail Error::NotConfiguredError.new unless configuration.api_key
+      fail Error::NotConfiguredError.new unless configuration.api_key || configuration.access_token
     end
 
   end
