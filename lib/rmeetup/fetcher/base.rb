f63ea45935f3584c89d79174852a02ef174389d5
Nikica Jokic <neektza@gmail.com>
2014-04-27 15:13:45 +0200

    Fetcher::Base auto-determines HTTP or HTTPS.
    Fetcher::Base#requester checks the query options and decides whether to
    use HTTP or HTTPS.
    
    Also, docs updated.

--- lib/rmeetup/fetcher/base.rb
+++ lib/rmeetup/fetcher/base.rb
@@ -1,5 +1,7 @@
 module RMeetup
   module Fetcher
+    Domain = "api.meetup.com"
+
     class ApiError < StandardError
       def initialize(error_message, request_url)
         super "Meetup API Error: #{error_message} - API URL: #{request_url}"
@@ -14,22 +16,22 @@ module RMeetup
 
     # == RMeetup::Fetcher::Base
     #
-    # Base fetcher class that other fetchers
-    # will inherit from.
+    # Base fetcher class that other fetchers will inherit from.
     class Base
       def initialize
         @type = nil
       end
 
-      # Fetch and parse a response
-      # based on a set of options.
-      # Override this method to ensure
-      # neccessary options are passed
-      # for the request.
+      # Fetch and parse a response based on a set of options.  Override this method to ensure neccessary options are passed for the request.
+      #
+      # @param [Hash] Options for deciding what and how to fetch
+      # @return [RMeetup::Collection] Collection of appropriate entities
       def fetch(options = {})
         path = path_and_query(options)
-        json = get_response(path)
-        data = JSON.parse(json)
+
+        # Fetch and parse data from Meetup
+        response_body = requester(options).get(path).body || raise(NoResponseError.new)
+        data = JSON.parse(response_body)
 
         # Check to see if the api returned an error
         raise ApiError.new(data['details'],path) if data.has_key?('problem')
@@ -39,35 +41,57 @@ module RMeetup
         # Format each result in the collection and return it
         collection.map!{|result| format_result(result)}
       end
+      
+      def path_and_query(options)
+        base_path + query(options)
+      end
+
+      def base_path
+        if @type.nil?
+          raise NotConfiguredError, /fetches only possible with a concrete fetcher, not with Base/
+        else
+          "/2/#{@type}"
+        end
+      end
+
+      # Create a query string from an options hash
+      def query(options)
+        '?' + URI.encode_www_form(options)
+      end
+      
+      # Decides whether to use HTTP or HTTPS. HTTPS is needed if we're authoizing via the :access_token
+      #
+      # @param [Hash] Constructed HTTP(S) query
+      # @return [Symbol] :http or :https
+      def requester(q)
+        if q.has_key?(:api_key)
+          http
+        elsif q.has_key?(:access_token)
+          https
+        else
+          http
+        end
+      end
 
       protected
-        # OVERRIDE this method to format a result section
-        # as per Result type.
-        # Takes a result in a collection and
-        # formats it to be put back into the collection.
-        def format_result(result)
-          result
-        end
+      # OVERRIDE this method to format a result section
+      # as per Result type.
+      # Takes a result in a collection and
+      # formats it to be put back into the collection.
+      def format_result(result)
+        result
+      end
 
-        def path_and_query(options)
-          base_path + params_for(options)
-        end
+      def http
+        Net::HTTP.new Domain, 80
+      end
 
-        def base_path
-          "/2/#{@type}/" unless @type.nil?
-        end
+      def https
+        c = Net::HTTP.new Domain, 443
+        c.use_ssl = true
+        c
+      end
 
-        # Create a query string from an options hash
-        def params_for(options)
-          '?' + URI.encode_www_form(options)
-        end
-
-        def get_response(path)
-          http = Net::HTTP.new "api.meetup.com", 443
-          http.use_ssl = true
-
-          http.get(path).body || raise(NoResponseError.new)
-        end
     end
   end
 end
